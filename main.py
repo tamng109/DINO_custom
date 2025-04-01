3388335bb2fc33e074bfde6f9f769da5a24f5b5c
tam1009 <trongtam.100902@gmail.com>
2025-04-01 23:51:29 +0700

    change image_set='test' for dataset_val

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def main(args):
     
 
     dataset_train = build_dataset(image_set='train', args=args)
-    dataset_val = build_dataset(image_set='val', args=args)
+    dataset_val = build_dataset(image_set='test', args=args)
 
     if args.distributed:
         sampler_train = DistributedSampler(dataset_train)
