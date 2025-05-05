df5c5ece04d57663e4f22d3b2f85b2665b1a83e1
tamng109 <trongtamng1009@gmail.com>
2025-05-05 13:53:25 +0700

    Update ms_deform_attn.py

--- models/dino/ops/modules/ms_deform_attn.py
+++ models/dino/ops/modules/ms_deform_attn.py
@@ -158,18 +158,18 @@ class MSDeformAttn(nn.Module):
                 imp = attention_weights.abs().sum(dim=[1,3,4])  # (N, n_heads)
                 self.head_importance += imp.sum(dim=0)
         # compute sampling locations as before
-        if reference_points.shape[-1] == 2:
+        # 1) Nếu ref_pts là [N, Len_q, 2], expand lên [N, Len_q, n_levels, 2]
+        if reference_points.dim() == 3 and reference_points.size(-1) == 2:
             reference_points = reference_points.unsqueeze(2).expand(-1, -1, self.n_levels, -1)
-            offset_norm = torch.stack([input_spatial_shapes[...,1], input_spatial_shapes[...,0]], -1)  # shape: (n_levels, 2)
-            offset_norm = offset_norm[None, None, None, :, None, :]  # (1, 1, 1, n_levels, 1, 2)
-            sampling_locs = reference_points[:, :, None, :, None, :] + sampling_offsets / offset_norm
-        elif reference_points.shape[-1] == 4:
-            # normalized offset
-            offset = sampling_offsets / self.n_points
-            # scale by width and height
-            sampling_locs = reference_points[:, :, None, :, None, :2] + offset * reference_points[:, :, None, :, None, 2:] * 0.5
+        # 2) Nếu ref_pts là [N, Len_q, n_levels, 2], giữ nguyên
+        elif reference_points.dim() == 4 and reference_points.size(-1) == 2:
+            pass
+        # 3) Nếu ref_pts là [N, Len_q, n_levels, 4], giữ nguyên
+        elif reference_points.dim() == 4 and reference_points.size(-1) == 4:
+            pass
         else:
-            raise ValueError(f"Unsupported reference_points shape: {reference_points.shape}")
+            raise ValueError(f"Unsupported reference_points shape {tuple(reference_points.shape)}")
+
         # deformable attention kernel
         if value.dtype == torch.float16:
             output = MSDeformAttnFunction.apply(value.to(torch.float32), input_spatial_shapes,
