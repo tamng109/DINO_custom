2bd3735b839b0b58e78c880294d607703e78162a
tamng109 <trongtamng1009@gmail.com>
2025-05-07 14:07:23 +0700

    Update coco_transformer.py

--- config/DINO/coco_transformer.py
+++ config/DINO/coco_transformer.py
@@ -1,4 +1,4 @@
-data_aug_scales = [480, 512, 576, 608, 640, 704, 768, 800]
+data_aug_scales = [480, 576, 640, 768, 800]
 data_aug_max_size = 1333
 data_aug_scales2_resize = [400, 500, 600]
 data_aug_scales2_crop = [384, 600]
